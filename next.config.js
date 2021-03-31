1e54ebb0857ec10e2099bace80eb08e3805ff739
Emiliano Parizzi <412489+empz@users.noreply.github.com>
2021-03-31 21:20:19 +0200

    Moved redirect rules to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
-const { i18n } = require('./next-i18next.config')
+const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   i18n,
-}
+  async redirects() {
+    return [
+      {
+        source: "/cita",
+        destination: "https://calendly.com/natachasouilhe",
+        permanent: true,
+      },
+      {
+        source: "/schedule",
+        destination: "https://calendly.com/natachasouilhe",
+        permanent: true,
+      },
+    ];
+  },
+};
